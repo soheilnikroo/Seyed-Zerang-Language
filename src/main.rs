128d574dcb40808aa675e60c8ed564cc812bd372
Soheil Nikroo <soheiln1234@gmail.com>
2026-03-25 15:56:27 +0330

    feat: add error handeling

--- src/main.rs
+++ src/main.rs
@@ -8,10 +8,24 @@ use parser::parse;
 use reader::read_source;
 use tokenize::tokenize;
 
+type Error = ();
+
+fn run() -> Result<(), Error> {
+    let source = read_source("some_file.zerang")?;
+    let tokens = tokenize(source)?;
+    let ast = parse(tokens)?;
+    let output = evaluate(ast)?;
+    Ok(())
+}
+
 fn main() {
     println!("Hello, Zerang!");
-    let source = read_source("some_file.zerang").unwrap();
-    let tokens = tokenize(source).unwrap();
-    let ast = parse(tokens).unwrap();
-    let output = evaluate(ast).unwrap();
+    match run() {
+        Ok(_) => {
+            println!("Success!")
+        }
+        Err(err) => {
+            eprintln!("It failed {err:?}")
+        }
+    }
 }
